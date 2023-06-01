3da75feeaddefdc7b17a407c366ae443854c26a0
Shoeb Shaikh <90shoeb@gmail.com>
2023-06-01 18:08:38 +0800

    model change

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from langchain.llms import HuggingFacePipeline
 from paperqa import Docs
 import streamlit as st
 
-model_name = "MBZUAI/LaMini-Neo-125M" #using an open-source model. 
+model_name = "MBZUAI/LaMini-Flan-T5-783M" #using an open-source model. 
 # You can choose a model of your choice available at https://huggingface.co/models
 embeddings = HuggingFaceEmbeddings()
 model = pipeline('text2text-generation', model = model_name)
@@ -15,7 +15,7 @@ docs = Docs(llm=local_llm, embeddings=embeddings)
 def get_docs(file_loc):    
     my_docs = [file_loc] # you can add other document paths based on the documents you are querying
     for d in set(my_docs):
-        docs.add(d,chunk_chars = 2000)
+        docs.add(d,chunk_chars = 3000)
 
 def get_response(query):     
     answer = docs.query(query)
